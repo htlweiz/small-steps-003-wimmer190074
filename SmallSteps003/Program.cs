def0e6972fa4dd8fba4a260bdd8b5001a9e809b8
Jan <Jan@DESKTOP-0DH4M1Q>
2020-05-14 11:15:07 +0200

    Fertig

--- SmallSteps003/Program.cs
+++ SmallSteps003/Program.cs
@@ -14,15 +14,15 @@ namespace SmallSteps003
 
         public static void Main(string[] args)
         {
-            result_addition = 0; // should be: a + b;
-            result_substraction = 0;
-            result_multiply = 0;
-            result_division = 0;
+            result_addition = a + b; // should be: a + b;
+            result_substraction = a - b;
+            result_multiply = a * b;
+            result_division = a / b;
 
             Console.WriteLine("Der Rechenking sagt {0} + {1} = {2}", a, b, result_addition);
             Console.WriteLine("Der Rechenking sagt {0} - {1} = {2}", a, b, result_substraction);
-            Console.WriteLine("Der Rechenking sagt {0} * {1} = {2}", a, b, 0);
-            Console.WriteLine("Der Rechenking sagt {0} / {1} = {2}", a, b, 0);
+            Console.WriteLine("Der Rechenking sagt {0} * {1} = {2}", a, b, result_multiply);
+            Console.WriteLine("Der Rechenking sagt {0} / {1} = {2}", a, b, result_division);
         }
     }
 }
